82d054562bb2969abafd06105e42e7fcefee22a9
Leandro Lourenci <leandro@lourenci.com>
2017-12-23 18:22:39 -0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const extractSass = new ExtractTextPlugin({
+  filename: 'app.bundle.[contenthash].css',
+  disable: process.env.NODE_ENV === 'development'
+})
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.[hash].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'index.html') }),
+    extractSass
+  ],
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [
+            { loader: 'css-loader' },
+            { loader: 'sass-loader' }
+          ]
+        })
+      },
+      { test: /\.(woff|woff2|eot|ttf)$/, loader: 'file-loader' },
+      { test: /\.svg$/, loader: 'file-loader' }
+    ]
+  }
+}
+
+module.exports = config
