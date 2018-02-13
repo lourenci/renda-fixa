8938b7b74a7559fa69bb779a207f3d38dec3eb5a
Leandro Lourenci <leandro@lourenci.com>
2018-02-13 11:58:23 -0200

    Refactor Investment Grid component

---
+++ src/components/App/components/InvestmentList/components/InvestmentGrid/components/InvestimentResult/components/InformationTable/index.js
@@ -0,0 +1,35 @@
+import style from './index.scss'
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class InformationTable extends React.Component {
+  render () {
+    const lines = () => (
+      this.props.lines.map((line, index) => (
+        <tr key={index}>
+          <td>{line.title}</td>
+          <td className='text-right'>{line.value}</td>
+        </tr>
+      ))
+    )
+
+    return (
+      <table className={`${style.table} w-100`}>
+        <tbody>
+          {lines()}
+        </tbody>
+      </table>
+    )
+  }
+}
+
+InformationTable.propTypes = {
+  lines: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired
+}
+
+export default InformationTable
